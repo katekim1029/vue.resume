c58d7021af76d638f49ff83ef3ac7d5b1bff5e2d
katekim <>
2021-01-30 19:08:11 +0900

    vue-router

--- src/main.js
+++ src/main.js
@@ -1,24 +1,27 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueRouter from 'vue-router'
 
 Vue.config.productionTip = false
+
+Vue.use(VueRouter)
 
 const Login = { template: '<div class="login">Login Page</div>' }
 const NotFound = { template: '<div class="notfound">Page not found</div>' }
 
-const routes = {
-  '/' : App,
-  '/login' : Login
-}
+const router = new VueRouter({
+  mode: 'history',
+  routes : [
+    { path: '/', component: App },
+    { path: '/login', component: Login },
+    { path: '*', component: NotFound }
+  ]
+})
 
 new Vue({
-  computed: {
-    VueComponent() {
-      return routes[window.location.pathname] || NotFound
-    }
-  },
+  router,
   //render: h => h(App),
   render(h) {
-    return h(this.VueComponent)
+    return h({ template: '<router-view></router-view>'})
   }
 }).$mount('#app')
